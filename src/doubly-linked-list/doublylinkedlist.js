d63131ded84f16564d2eb4f6f0c030e4afa1e49f
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-10-11 21:49:44 +0200

    Add push() method

--- src/doubly-linked-list/doublylinkedlist.js
+++ src/doubly-linked-list/doublylinkedlist.js
@@ -1,5 +1,6 @@
 import DoubleNode from './doublenode.js';
 
+// Higher memory consumption, faster iteration than SLL
 class DoublyLinkedList {
 
     constructor() {
@@ -7,6 +8,24 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+
+    push(value) {
+        let newTail = DoubleNode(value)
+        if (this.length === 0) {
+            this.head = this.tail = newNode;
+        }
+        this.tail.next = newTail;
+        newTail.previous = this.tail;
+        this.tail = newTail;
+        this.length++;
+        return this;
+    }
+
 }
+
+
+let dll = new DoublyLinkedList();
+
 
 export default DoublyLinkedList;
