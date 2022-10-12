291faa2fca903d4a6b4e95f07ec4cb3ece0965a0
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-10-12 22:27:48 +0200

    Add shift() method. Fix inconsistency in returned node in pop()

--- src/doubly-linked-list/doublylinkedlist.js
+++ src/doubly-linked-list/doublylinkedlist.js
@@ -28,6 +28,7 @@ class DoublyLinkedList {
         }
         let oldTail = this.tail;
         this.tail = oldTail.previous;
+        oldTail.previous = null;
         if (!this.tail ) { // i.e. tail is now null or the DLL contains single node
             this.head = this.tail;
             length--;
@@ -37,6 +38,26 @@ class DoublyLinkedList {
         this.length--;
         return oldTail;
     }
+
+
+    shift() {
+        if (this.length == 0) {
+            return null;
+        }
+        let oldHead = this.head;
+        let newHead = this.head.next;
+        this.head = newHead;
+        if (!this.head) {
+            this.length--;
+            this.tail = this.head;
+            return oldHead;
+        }
+        oldHead.next = null;
+        newHead.previous = null;
+        this.length--;
+        return oldHead;
+    }
+
 
     printAllNodes() {
         let currentNode = this.head;
@@ -57,6 +78,19 @@ dll.push('First');
 dll.push('Second');
 dll.printAllNodes();
 dll.pop();
+console.log('Popped once...');
+dll.printAllNodes();
+dll.pop();
+console.log('Popped twice...');
+dll.printAllNodes();
+dll.push('First');
+dll.push('Second');
+dll.push('Third');
+console.log('Pushing three');
+dll.printAllNodes();
+dll.shift();
+dll.shift();
+console.log('Shifting twice:');
 dll.printAllNodes();
 
 
