233fdfa76c733ed3310441e17bfa0b3beaefd225
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-10-19 21:49:20 +0200

    Debug hasSameDigitFrequency() and add validation of function

--- src/optional-challenges/frequencycounter.js
+++ src/optional-challenges/frequencycounter.js
@@ -10,21 +10,38 @@ function hasSameDigitFrequency(num1, num2) {
         return false;
     }
 
-    frequencyTable1 = {}
-    frequencyTable2 = {}
+    let frequencyTable1 = {}
+    let frequencyTable2 = {}
 
     for (let i = 0; i < num1.length; i++) {
         frequencyTable1[num1[i]] = (frequencyTable1[num1[i]] || 0) + 1;
         frequencyTable2[num2[i]] = (frequencyTable2[num2[i]] || 0) + 1;
     }
     
-    for (let digit of frequencyTable1) {
+    for (let digit of num1) {
         if (!(digit in frequencyTable2)) {
             return false;
         }
-        if (frequencyTable1[digit] != frequencyTable2[digit]) {
+        if (frequencyTable1[digit] !== frequencyTable2[digit]) {
             return false;
         }
     }
     return true;
 }
+
+
+// TODO: Export to test cases
+console.log(hasSameDigitFrequency(1, 2));
+console.log(hasSameDigitFrequency(123, 321));
+console.log(hasSameDigitFrequency(2, 1));
+try {
+    console.log(hasSameDigitFrequency(null, 2));
+} catch(err) {
+    console.log(err + ' was caught!')
+}
+console.log(hasSameDigitFrequency(4356, 4652));
+console.log(hasSameDigitFrequency(6574839210, 1234567890));
+
+
+
+export default hasSameDigitFrequency;
