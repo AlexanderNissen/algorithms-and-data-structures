6faeddd659aebbd7645d7f4412f939d7460fb88a
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-10-26 22:22:20 +0200

    Add insertion sort

--- src/insertion-sort/insertionsort.js
+++ src/insertion-sort/insertionsort.js
@@ -1,11 +1,28 @@
 import generateIntArray from "../utils/generateintarray.js";
 
-
+/*
+A sorted subarray is built up, expanding the subarray by one for each iteration.
+*/
 function insertionSort(array) {
+    if (!array) {
+        return null;
+    }
+    let currentValue;
+    for (let i = 1; i < array.length; i++) {
+        currentValue = array[i]
+        if (currentValue > array[i - 1]) {
+            continue;
+        }
+        for (var j = i - 1; j >= 0 && array[j] > currentValue; j--) {            
+            array[j + 1] = array[j];
+        }
+        array[j + 1] = currentValue;
+    }
     return array;
 }
 
 
+// TODO: Export to test cases
 let arr1 = generateIntArray(5, 1000);
 let arr2 = generateIntArray(10, 1000);
 let arr3 = generateIntArray(20, 1000);
@@ -19,4 +36,4 @@ console.log(insertionSort(arr5));
 
 
 
-export default selectionSort;
+export default insertionSort;
