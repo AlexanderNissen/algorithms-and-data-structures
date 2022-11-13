cb03943feef30552d43ec8767c44344dcd0aaa17
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-11-13 22:28:49 +0100

    Add quick sort

--- src/quick-sort/quicksort.js
+++ src/quick-sort/quicksort.js
@@ -3,8 +3,16 @@ import swap from '../utils/swap.js';
 import timeThis from '../utils/timethis.js';
 
 
-function quicksort(array) {
-
+function quickSort(array, left = 0, right = array.length - 1) {
+    if (!array) {
+        return null;
+    }
+    if (left < right) {
+        let partitionIndex = partition(array, left, right);
+        quickSort(array, left, partitionIndex - 1);
+        quickSort(array, partitionIndex + 1, right);
+    }
+    return array;
 }
 
 /*
@@ -14,19 +22,33 @@ LHS of the pivot while maintaining the values greater than the pivot value
 on the RHS
 */
 
-function partition(array) {
-    let pivot = array[0];
-    let lessThan = 0;
-    for (let i = 1; i < array.length; i++) {
-        if (array[i] <= pivot) {
-            console.log(`Swapping ${array[lessThan + 1]} with ${array[i]}`);
-            swap(array, lessThan + 1, i);
-            lessThan++;
+function partition(array, start = 0, end = array.length - 1) {
+    let pivot = array[start];
+    let swapIndex = start;
+    for (let i = start + 1; i <= end; i++) {
+        if (array[i] < pivot) {
+            swapIndex++;
+            swap(array, swapIndex, i);
         }
     }
-    console.log(`Moving forward the pivot of ${pivot}`);
-    swap(array, 0, lessThan);
+    swap(array, start, swapIndex);
+    return swapIndex;
 }
 
-let array = [3, 4, 2, 1, 5, 10, 12, 7, 6, 9];
-partition(array);
+
+let arr1 = generateIntArray(5, 10000);
+let arr2 = generateIntArray(10, 10000);
+let arr3 = generateIntArray(20, 10000);
+let arr4 = generateIntArray(50, 10000);
+let arr5 = generateIntArray(100, 10000);
+let arr6 = generateIntArray(1000, 10000);
+console.log(quickSort(arr1));
+console.log(quickSort(arr2));
+console.log(quickSort(arr3));
+console.log(quickSort(arr4));
+console.log(quickSort(arr5));
+console.log(quickSort(arr6));
+
+
+
+export default quickSort;
