c7fd71ac619880b3f83d7148d96beb48de8276e8
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-11-14 22:21:53 +0100

    Add radix sort

--- src/sorting/radix-sort/radixsort.js
+++ src/sorting/radix-sort/radixsort.js
@@ -1,5 +1,6 @@
 import generateIntArray from '../../utils/generateintarray.js';
 
+
 /*
     Runs through the first digit for all integer elements in the array,
     ordering the numbers by putting them in bins based on the value of their
@@ -10,7 +11,16 @@ import generateIntArray from '../../utils/generateintarray.js';
     until the array is sorted.
 */
 function radixSort(array) {
-
+    let maxDigits = getMostDigits(array);
+    for (let digitPos = 0; digitPos < maxDigits; digitPos++) {
+        let digitBuckets = Array.from({length: 10}, () => []);
+        for (let int of array) {
+            let bucketNumber = getDigit(int, digitPos);
+            digitBuckets[bucketNumber].push(int);
+        }
+        array = [].concat(...digitBuckets);
+    }
+    return array;
 }
 
 /*
@@ -20,6 +30,14 @@ function radixSort(array) {
 */
 function getDigit(int, posFromRight) {
     return Math.floor(Math.abs(int) / Math.pow(10, posFromRight)) % 10;
+}
+
+function getMostDigits(array) {
+    let maxDigits = 0;
+    for (let int of array) {
+        maxDigits = Math.max(countDigits(int), maxDigits);
+    }
+    return maxDigits;
 }
 
 function countDigits(int) {
@@ -50,6 +68,23 @@ console.log(countDigits(int1));
 console.log(countDigits(int2));
 console.log(countDigits(int3));
 console.log(countDigits(int4));
+
+let arr1 = generateIntArray(1000, 100);
+let arr2 = generateIntArray(1000, 1000);
+let arr3 = generateIntArray(1000, 10000);
+let arr4 = generateIntArray(1000000, 100000);
+
+console.log('--- getMostDigits ---');
+console.log(getMostDigits(arr1));
+console.log(getMostDigits(arr2));
+console.log(getMostDigits(arr3));
+console.log(getMostDigits(arr4));
+
+console.log('--- radixSort ---');
+console.log(radixSort(arr1));
+console.log(radixSort(arr2));
+console.log(radixSort(arr3));
+console.log(radixSort(arr4));
 
 
 
