694a6b616c95450aef9b1a83a781ca809aed8b7c
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-11-16 22:11:17 +0100

    Add TreeNode and BinarySearchTree

---
+++ src/data-structures/tree/binary-search-tree.js
@@ -0,0 +1,19 @@
+import TreeNode from "./tree-node.js";
+
+class BinarySearchTree {
+
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        return undefined;
+    }
+}
+
+
+let bst = new BinarySearchTree();
+
+
+
+export default BinarySearchTree;
