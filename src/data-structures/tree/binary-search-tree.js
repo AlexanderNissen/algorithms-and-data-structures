8899ca9e0cfce65db435d13257342195c5d552a3
AlexanderNissen <alexandernissen.mail@gmail.com>
2022-11-17 22:12:29 +0100

    Add insert() to BST

--- src/data-structures/tree/binary-search-tree.js
+++ src/data-structures/tree/binary-search-tree.js
@@ -6,13 +6,42 @@ class BinarySearchTree {
         this.root = null;
     }
 
+    // This can obviously be done more elegantly
     insert(value) {
-        return undefined;
+        let newNode = new TreeNode(value); 
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.left;
+                    }
+                } else if (value > currentNode.value) {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    } else {
+                        currentNode = currentNode.right;
+                    }
+                } else {
+                    throw "Inserted value must not be duplicate of existing value";
+                }
+            }
+        }
     }
 }
 
 
 let bst = new BinarySearchTree();
+bst.insert(5);
+bst.insert(3);
+bst.insert(2);
+bst.insert(10);
 
 
 
