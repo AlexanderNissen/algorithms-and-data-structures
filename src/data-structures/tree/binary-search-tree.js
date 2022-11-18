3669117306954da25f0e513c4c72514245d1f19c
Alexander Nissen <alni@netcompany.com>
2022-11-18 16:05:09 +0100

    Add contains() to binary search tree

--- src/data-structures/tree/binary-search-tree.js
+++ src/data-structures/tree/binary-search-tree.js
@@ -34,14 +34,37 @@ class BinarySearchTree {
             }
         }
     }
+
+    find(value) {
+        let currentNode = this.root;
+        while(currentNode) {
+            if (value === currentNode.value) {
+                return true;
+            } else if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else {
+                currentNode = currentNode.right;
+            }
+        }
+        return false;
+    }
 }
 
 
+// TODO: Export to test cases
 let bst = new BinarySearchTree();
+console.log('Inserting 5, 3, 2, and 10 in given order')
 bst.insert(5);
 bst.insert(3);
 bst.insert(2);
 bst.insert(10);
+console.log('Finding 0, 4, 11, 5, and 2:');
+console.log(bst.find(0));
+console.log(bst.find(4));
+console.log(bst.find(11));
+console.log(bst.find(5));
+console.log(bst.find(2));
+console.log(bst.find(10));
 
 
 
